928d34ac03e1a68ebd24f9465749ad62c1287468
Johann Nicolás Nieto Cárdenas <johann.nieto@carvajal.com>
2024-07-18 19:48:02 -0500

    first commit

---
+++ src/main/java/com/schoolmaster/userservice/SchoolMasterUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.schoolmaster.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolMasterUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolMasterUserServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/schoolmaster/userservice/SchoolMasterUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schoolmaster.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolMasterUserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
